53b9b22fb4fe0be4b097843aded6d9c321095fa1
AngelBB10 <angelbarrasbarreira@gmail.com>
2022-02-16 19:08:26 +0100

    Loop Exercises

---
+++ exercise04.py
@@ -0,0 +1,15 @@
+#Loop Fun 
+
+print('Loop 1')
+for i in range(10):
+    if i < 5: 
+         print('Pikachu')
+    else: 
+        print('Venomat')
+
+print('Loop 2')
+for i in range(10):
+    if i >= 5: 
+         print('Pikachu')
+    else: 
+        print('Venomat')
