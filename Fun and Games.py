9555a07c9f21686c67efed14941a845c061ae256
AngelBB10 <angelbarrasbarreira@gmail.com>
2022-02-16 18:44:15 +0100

    Formatted File

--- Fun and Games.py
+++ Fun and Games.py
@@ -6,4 +6,4 @@ bread = 1
 pasta = 1.5
 burger = 5.55
 house = 10000.956
-print (house+burger+bread+pasta+pizza+burger+bread+dog+house*12)
+print (house + burger + bread + pasta + pizza + burger + bread + dog + house * 12)
